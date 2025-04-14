c8336d1ae20e7a66c76b4b7c3eae909d2cc3329a
axwelrafael@gmail.com <axwelrafael@gmail.com>
2025-04-14 20:15:47 -0300

    Atividade7

---
+++ Mod7/src/Veiculo.java
@@ -0,0 +1,66 @@
+public class Veiculo {
+    //Uma classe para Veiculos Terrestres e os metodos Getter e Setter delas.
+    private String tipo;
+    private int eixos;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String tipoCombustivel;
+    private float preco;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEixos() {
+        return eixos;
+    }
+
+    public void setEixos(int eixos) {
+        this.eixos = eixos;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}

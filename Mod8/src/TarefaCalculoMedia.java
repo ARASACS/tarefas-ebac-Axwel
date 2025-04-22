bd66f0ef656a5fbb4cd8915c89929b6eb3f2ff20
axwelrafael@gmail.com <axwelrafael@gmail.com>
2025-04-22 00:03:59 -0300

    Atividade8Correta

---
+++ Mod8/src/TarefaCalculoMedia.java
@@ -0,0 +1,19 @@
+public class TarefaCalculoMedia {
+    public static void main(String[] args) {
+    CalculoMedia();
+
+    }
+    public static void CalculoMedia(){
+        int num1 = 8;
+        int num2 = 7;
+        int num3 = 6;
+        int num4 = 5;
+        System.out.println("Número 1: " + num1);
+        System.out.println("Número 2: " + num2);
+        System.out.println("Número 3: " + num3);
+        System.out.println("Número 4: " + num4);
+
+        System.out.println("Media dos Números: " +(num1 + num2 + num3 + num4 / 4));
+
+    }
+}
